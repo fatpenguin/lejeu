2b6a89f31db81e48b18a2aa5e59d9baec119eb6d
David Robertaud <drobertaud@uk-c02x5246jg5j.uk.deloitte.com>
2019-06-18 14:05:01 +0100

    move an image

--- main.cpp
+++ main.cpp
@@ -2,21 +2,47 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+    window.setFramerateLimit(60);
+
+    // Load textures
+    sf::Texture texture;
+    texture.loadFromFile("assets/image1.jpeg");
+
+    sf::Sprite sprite(texture);
+    sf::Event event;
 
     while (window.isOpen())
     {
-        sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
+            {
                 window.close();
+            }
+            else if (event.type == sf::Event::KeyPressed)
+            {
+                switch (event.key.code)
+                {
+                case (sf::Keyboard::Up):
+                    sprite.move(0, -5.f);
+                    break;
+                case (sf::Keyboard::Down):
+                    sprite.move(0, 5.f);
+                    break;
+                case (sf::Keyboard::Left):
+                    sprite.move(-5.f, 0);
+                    break;
+                case (sf::Keyboard::Right):
+                    sprite.move(5.f,0);
+                    break;
+                default:
+                    break;
+                }
+            }
         }
-
         window.clear();
-        window.draw(shape);
+        window.draw(sprite);
         window.display();
     }
 
